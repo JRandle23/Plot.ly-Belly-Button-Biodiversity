e841b783ab2c9aa9f306800b21fc112b7658c54d
JRandle23 <jenny.randle23@gmail.com>
2021-03-15 13:40:40 -0700

    adding bar chart code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,13 +1,19 @@
-function getPlot(id) {
-
+function buildBarChart(sample) {
     d3.json("samples.json").then((data) => {
-        console.log(data)
+        console.log(data);
+        var samples = data.samples;
+        var resultsList = samples.filter(sampleObj => sampleObj.id == sample);
+        var results = resultsList[0];
 
-        var 
+        var sampleValues = results.sample_values;
+        var otuIDs = results.otu_ids;
+        var otuLabels = results.otu_labels;
+
+
     });
-
-
-
-
-
 }
+
+
+
+
+
